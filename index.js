9b624fa52f302ced057dc647ffa5408b3b475d7d
Darin Nguyen <danguyen01@gmail.com>
2019-10-26 01:09:24 -0700

    Cleaned up code

--- index.js
+++ index.js
@@ -25,12 +25,9 @@ bot.on("message", async message => {
         let msg = message.content.slice(config.prefix.length); // slice of the prefix on the message
 
         let args = msg.split(" "); // break the message into part by spaces
-
-        let cmd = args[0].toLowerCase(); // set the first word as the command in lowercase just in case
-
         args.shift(); // delete the first word from the args
-
-
+        let cmd = args[0].toLowerCase(); // set the first word as the command in lowercase just in case
+        /*
         if (cmd === 'hi' || cmd === 'hello') { // the first command [I don't like ping > pong]
             message.channel.send(`Hi there ${message.author.toString()}`);
             return;
@@ -40,16 +37,11 @@ bot.on("message", async message => {
             return message.channel.send('pong');
         }
 
-        // Make sure this command always checks for you. YOU NEVER WANT ANYONE ELSE TO USE THIS COMMAND
-        else if (cmd === "eval" && message.author.id === config.owner){ // < checks the message author's id to yours in config.json.
-            const code = args.join(" ");
-            return evalCmd(message, code);
-        }
-
         else { // if the command doesn't match anything you can say something or just ignore it
             message.channel.send(`I don't know what command that is.`);
             return;
         }
+        */
 
     } else if (message.content.indexOf("<@"+bot.user.id) === 0 || message.content.indexOf("<@!"+bot.user.id) === 0) { // Catch @Mentions
 
@@ -57,28 +49,6 @@ bot.on("message", async message => {
     }
     return;
 });
-
-function evalCmd(message, code) {
-    if(message.author.id !== config.owner) return;
-    try {
-        let evaled = eval(code);
-        if (typeof evaled !== "string")
-            evaled = util.inspect(evaled);
-            message.channel.send(clean(evaled), {code:"xl"});
-    } catch (err) {
-        message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-    }
-}
-function clean(text) {
-    if (typeof(text) !== 'string') {
-        text = util.inspect(text, { depth: 0 });
-    }
-    text = text
-        .replace(/`/g, '`' + String.fromCharCode(8203))
-        .replace(/@/g, '@' + String.fromCharCode(8203))
-        .replace(config.token, 'mfa.VkO_2G4Qv3T--NO--lWetW_tjND--TOKEN--QFTm6YGtzq9PH--4U--tG0') //Don't let it post your token
-    return text;
-}
 
 // Catch Errors before they crash the app.
 process.on('uncaughtException', (err) => {
