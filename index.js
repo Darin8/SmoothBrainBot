d1f051ed6cedb16b39c19927ab8381764a8572a1
Darin Nguyen <danguyen01@gmail.com>
2019-10-27 02:36:54 -0700

    Added gelbooru functionality

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const config = require('./config.json');
 const Discord = require('discord.js');
 const util = require('util');
+const Danbooru = require('danbooru')
 const irc_client = require('./irc.js');
 const bot = new Discord.Client({
     disableEveryone: true,
     disabledEvents: ['TYPING_START']
 });
+
+const multiEmbed = new Discord.RichEmbed()
+    .setColor('#f988ed')
+    .setTitle('Weekly osu!std lobby')
+    .setDescription('The weekly osu!std lobby is up!')
+    .addField('Room Name:', "osu!UCISDRBLA Standard", false)
+    .addField('Password:', "maxisagod", true)
+    .setFooter('<@&637950916934238218>')
 
 bot.on("ready", () => {
     console.log(`Bot is online!\n${bot.users.size} users, in ${bot.guilds.size} servers connected.`);
@@ -15,11 +24,6 @@ bot.on("ready", () => {
 bot.on("message", async message => {
 
     if(message.author.bot || message.system) return;
-    /*
-    if(message.channel.type === 'dm') { // Direct Message
-        return; //Optionally handle direct messages
-    }
-    */
 
     if (message.content.indexOf(config.prefix) === 0) {
         console.log(message.author.username,': ',message.content);
@@ -34,20 +38,41 @@ bot.on("message", async message => {
             message.channel.send(`Hi there ${message.author.toString()}`);
             return;
         }
+        else if (cmd === 'dan') {
+          const booru = new Danbooru()
+          booru.posts({ tags: 'rating:safe order:rank' }).then(posts => {
+            // Select a random post from posts array
+            const index = Math.floor(Math.random() * posts.length)
+            const post = posts[index]
 
+            // Get post's url and create a filename for it
+            const url = booru.url(post.file_url)
+            const name = `${post.md5}.${post.file_ext}`
+            message.channel.send(url.href);
+          })
+        }
+        else if(cmd === 'open'){
+            irc_client.open();
+            message.channel.send(multiEmbed);
+        }
+        else if(cmd === 'close'){
+            message.channel.send('Closing...');
+            irc_client.close();
+            message.channel.send('Done.');
+        }
         else if (cmd === 'ping') { // ping > pong just in case..
             return message.channel.send('pong');
         }
 
-        else { // if the command doesn't match anything you can say something or just ignore it
+        else {
             message.channel.send(`I don't know what command that is.`);
             return;
         }
 
 
-    } else if (message.content.indexOf("<@"+bot.user.id) === 0 || message.content.indexOf("<@!"+bot.user.id) === 0) { // Catch @Mentions
+    } else if (message.content.indexOf("<@"+bot.user.id) === 0 || message.content.indexOf("<@!"+bot.user.id) === 0) {
 
-        return message.channel.send(`Use \`${config.prefix}\` to interact with me.`); //help people learn your prefix
+        return message.channel.send(`Use \`${config.prefix}\` to interact with me.`);
     }
     return;
 });
--- irc.js
+++ irc.js
@@ -9,24 +9,26 @@ const client = new Banchojs.BanchoClient({
     apiKey: config.irc.apiKey
 });
 
-client.connect().then(async () => {
-    console.log("Connected to Bancho.")
-    const channel = await client.createLobby("osu!ucisdrbla");
-    lobby = channel.lobby;
-    lobby.setPassword(config.irc.lobby_password);
-    console.log("Lobby created! Name: "+lobby.name+", password: "+ config.irc.lobby_password);
-    console.log("Multiplayer link: https://osu.ppy.sh/mp/"+lobby.id);
+exports.open = function(){
+    client.connect().then(async () => {
+        console.log("Connected to Bancho.")
+        const channel = await client.createLobby("osu!UCISDRBLA Standard");
+        lobby = channel.lobby;
+        lobby.setPassword(config.irc.lobby_password);
+        console.log("Lobby created! Name: "+lobby.name+", password: "+ config.irc.lobby_password);
+        console.log("Multiplayer link: https://osu.ppy.sh/mp/"+lobby.id);
 
-    // Give host to first person
-    lobby.once("playerJoined", (obj) => {
-        lobby.setHost(obj.player.user.username);
-    });
-}).catch(console.error);
+        // Give host to first person
+        lobby.once("playerJoined", (obj) => {
+            lobby.setHost(obj.player.user.username);
+        });
 
-process.on("SIGINT", async () => {
+    }).catch(console.error);
+};
+
+exports.close = async function(){
     console.log("Closing lobby and disconnecting...");
     await lobby.closeLobby();
     await client.disconnect();
     console.log("Done")
-});
-
+}
